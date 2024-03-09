15e15bda43aab2980dcee3fe8052a2aaa9df6f8c
Suyank Saraswat <suyank.saraswat@gmail.com>
2024-03-10 00:41:58 +0530

    feat: setup husky, lint staged and commitizen

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,12 @@
 // prettier.config.js
 module.exports = {
   plugins: ['prettier-plugin-tailwindcss'],
-  tailwindFunctions: ['clsx', 'cn', 'twMerge']
+  tailwindFunctions: ['clsx', 'cn', 'twMerge'],
+  singleQuote: true,
+  arrowParens: 'always',
+  trailingComma: 'none',
+  jsxBracketSameLine: true,
+  printWidth: 100,
+  tabWidth: 2,
+  semi: false
 };
